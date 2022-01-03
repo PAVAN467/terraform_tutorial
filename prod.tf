0f52b9dd2598aa1c27eef3da3fac3e56f409d051
PavanKumar <pavankumar.cs17@bmsce.ac.in>
2022-01-04 01:46:49 +0530

    Defined initial web security group

--- prod.tf
+++ prod.tf
@@ -1,19 +1,50 @@
 provider "aws" {
- profile = "default"
- region ="ap-south-1"
+ profile  = "default"
+ region   = "ap-south-1"
 
 }
 resource "aws_s3_bucket" "prod_tf_course" {
-  bucket = "tf-course-firstone"
-  acl ="private"
+  bucket  = "tf-course-firstone"
+  acl     ="private"
  
 }
 
 resource "aws_default_vpc" "default" {
 
+}
+
+resource "aws_security_group" "prod_web" {
+ name         ="prodweb"
+ description  ="Allow all standard http and https for inbound and everything for outbound"
+
+ ingress {
+
+    from_port  = 80
+    to_port    = 80
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+   }
+ ingress {
+  
+   from_port  =  443
+   to_port    =  443
+   protocol   =  "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
 
 
+    }
+ egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+     }
 
 
+tags ={
+"Terraform" : "true"
+ }
 
 }
+
